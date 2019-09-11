f4aeb7566c7db4e6c1dd3e2873adaa5a4eaee0a7
mayaliberman <50631141+mayaliberman@users.noreply.github.com>
2019-09-11 11:24:02 +0300

    Updated error handling

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {data} = require('./data/data.json');
-const {projects} = data;
-
+const { data } = require('./data/data.json');
+const { projects } = data;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,43 +10,47 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
-
-app.get('/', (req,res) => {
-       res.render('index', {projects})
+app.get('/', (req, res) => {
+  res.render('index', { projects });
 });
 
-app.get('/about', (req,res) => {
-    res.render('about')
-})
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.get('/projects/:id', (req,res) => {
-    const { id } = req.params;
-    const projectTitle = projects[id-1].project_name
-    const projectDescription = projects[id-1].description
-    const projImgs = projects[id-1].img_proj;
-    const liveLink = projects[id-1].live_link;
-    const githubLink = projects[id-1].github_link;
-    const technologies = projects[id-1].technologies;
-    const projectData = {id, projectTitle, projImgs, projectDescription,  liveLink, githubLink, technologies, projects}
-    res.render(`project`, projectData)
-    
-})
+app.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const projectTitle = projects[id - 1].project_name;
+  const projectDescription = projects[id - 1].description;
+  const projImgs = projects[id - 1].img_proj;
+  const liveLink = projects[id - 1].live_link;
+  const githubLink = projects[id - 1].github_link;
+  const technologies = projects[id - 1].technologies;
+  const projectData = {
+    id,
+    projectTitle,
+    projImgs,
+    projectDescription,
+    liveLink,
+    githubLink,
+    technologies,
+    projects
+  };
+  res.render(`project`, projectData);
+});
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err)
-  });
-  
-  app.use((err, req, res, next) => {
-    res.locals.error = err
-    //Refernce to the solution: https://teamtreehouse.com/community/rangeerror-invalid-status-code-undefined-when-linking-to-another-page
-    if(err.status >= 100 && err.status < 600) {
-      res.status(err.status);
-    } else {
-      res.render('error'); 
-    }
-  })
+  const err = new Error('Not Found');
+  err.status = 404;
+  console.error('The page is not found status',err.status)
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  console.error('This page was not created yet! page status is',err.status)
+  res.render('error', err);
+});
 
 app.listen(8080, () => {
   console.log('The application is running on localhost 8080');
